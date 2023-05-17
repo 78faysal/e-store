3d52788c5a8a396ec5324dbed5954585a7163bb4
mdfaysal <faysal7830@gmail.com>
2023-05-17 21:44:20 +0600

    cards added

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,6 +4,7 @@ const Products = () => {
 
     const [products, setProduct] = useState([]);
     const [activeTab, setActiveTab] = useState(1);
+    const [bags, setBags] = useState([]);
 
     useEffect(() => {
         fetch('./fakeData.json')
@@ -44,27 +45,26 @@ const Products = () => {
                 </div>
                 <div className="w-full max-w-sm">
                     {activeTab === 1 &&
-                        <div>
+                        <div  className="flex flex-wrap justify-center">
                             {
-                                products && products.map(product => { 
-                                    const bags = product?.category === 'Bag'; 
-                                    if (bags) {
-                                        // console.log('true')
-                                        <div class="card w-96 bg-base-100 shadow-xl">
-                                            <figure class="px-10 pt-10">
-                                                <img src={product?.img} alt="Shoes" class="rounded-xl" />
-                                            </figure>
-                                            <div class="card-body items-center text-center">
-                                                <h2 class="card-title">{product?.name}</h2>
-                                                <p>If a dog chews shoes whose shoes does he choose?</p>
-                                                <div class="card-actions">
-                                                    <button class="btn btn-primary">Buy Now</button>
+                                products && products.map(product => {
+
+                                    if (product?.category === 'Bag') {
+                                        // console.log(product)
+                                        return (
+                                            <div className="card w-96 bg-base-100 shadow-xl">
+                                                <figure className="px-10 pt-10">
+                                                    <img src={product?.img} alt="Shoes" class="rounded-xl" />
+                                                </figure>
+                                                <div class="card-body items-center text-center">
+                                                    <h2 class="card-title">{product?.name}</h2>
+                                                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                    <div class="card-actions">
+                                                        <button class="btn btn-primary">Buy Now</button>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    }
-                                    else{
-                                        console.log("Please select ")
+                                        )
                                     }
                                 })
                             }
