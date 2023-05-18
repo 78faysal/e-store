fbca94a4bd1a824f73c2c835dda4dd210538c131
mdfaysal <faysal7830@gmail.com>
2023-05-18 22:52:28 +0600

    testimonials section added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,31 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const Header = () => {
+
+    const brands = [
+        {
+            id: 1,
+            name: 'Apple',
+            image: 'https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png',
+        },
+        {
+            id: 2,
+            name: 'Samsung',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBoxsX3bw93gVfimwJZkunsRbDWxraySQjbJV89johRJRw8qL1&s',
+        },
+        {
+            id: 3,
+            name: 'Adidas',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweocjhoj41IjjcEWXFZKK33SGuePNYpN5ksznQ32RoGcTxEc&s',
+        },
+        {
+            id: 2,
+            name: 'Gucci',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwvtCsJ60bkvm1zRV4_pU-GNVRrsy8YIKkLYnFekzR7QK6A2Q&s',
+        } 
+        // Add more brands here
+    ];
+
     return (
         <div className='mt-20'>
             <>
@@ -24,7 +49,7 @@ const Header = () => {
                 >
                     <SwiperSlide>
                         <div className='relative'>
-                            <img style={{height: "600px"}} className='w-full object-cover brightness-75' src={img1} alt="" />
+                            <img style={{ height: "600px" }} className='w-full object-cover brightness-75' src={img1} alt="" />
                             <div className='absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                                 <p className='text-xl font-semibold'>In this season, find the best 🔥</p>
                                 <h2 className='text-6xl font-bold mb-4'>Exclusive collection for everyone</h2>
@@ -34,28 +59,40 @@ const Header = () => {
                         </div>
                     </SwiperSlide>
                     <SwiperSlide><div className='relative'>
-                        <img style={{height: "600px"}} className='h-2/5 w-full object-cover brightness-75' src={img2} alt="" />
+                        <img style={{ height: "600px" }} className='h-2/5 w-full object-cover brightness-75' src={img2} alt="" />
                         <div className='absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                             <p className='text-xl font-semibold'>In this season, find the best 🔥</p>
                             <h2 className='text-6xl font-bold mb-4'>Exclusive collection for everyone</h2>
 
-                            <button className='btn rounded-full border-yellow-300'>Explore Now</button>
+                            <button className='btn rounded-full hover:border-yellow-300'>Explore Now</button>
                         </div>
                     </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                    <div className='relative'>
-                            <img style={{height: "600px"}} className='h-2/5 w-full object-cover brightness-75' src={img3} alt="" />
+                        <div className='relative'>
+                            <img style={{ height: "600px" }} className='h-2/5 w-full object-cover brightness-75' src={img3} alt="" />
                             <div className='absolute text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                                 <p className='text-xl font-semibold'>In this season, find the best 🔥</p>
                                 <h2 className='text-6xl font-bold mb-4'>Exclusive collection for everyone</h2>
 
-                                <button className='btn rounded-full border-yellow-300'>Explore Now</button>
+                                <button className='btn rounded-full hover:border-yellow-300'>Explore Now</button>
                             </div>
                         </div>
                     </SwiperSlide>
                 </Swiper>
             </>
+
+            <div className="container mx-auto py-8">
+                <h2 className="text-3xl font-bold mb-4">Our Brands</h2>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {brands.map(brand => (
+                        <div key={brand.id} className="bg-white p-4 shadow-md rounded">
+                            <img src={brand.image} alt={brand.name} className="h-24 mx-auto" />
+                            <h3 className="text-lg font-semibold mt-2">{brand.name}</h3>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -8,20 +8,48 @@ import { Pagination } from "swiper";
 
 const Testimonials = () => {
     return (
-        <div>
+        <div className="bg-gray-50 p-10 py-16">
+            <h2 className="text-4xl text-center font-bold">Good news from far away 🥇</h2>
+            <p className="text-xl text-center text-gray-500 p-2 pb-4">Let's see what people think of E-Store</p>
             <Swiper
-                slidesPerView={3}
-                spaceBetween={30}
+                slidesPerView={2}
+                spaceBetween={20}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+                <div className="grid grid-cols-1 md:grid-cols-2 rounded-2xl  container mx-auto">
+                    <SwiperSlide>
+                        <div className="bg-white p-4 shadow-lg hover:shadow-xl rounded-xl text-center m-7">
+                            <h3 className="text-xl font-semibold mb-2">John Doe</h3>
+                            <h4 className="text-gray-600 text-sm mb-4">France</h4>
+                            <p className="text-gray-800">One of the standout features is the seamless navigation. The well-organized categories and search functionality make it effortless to find the items I'm looking for. The filters and sorting options further enhance the browsing experience.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="bg-white p-4 shadow-lg hover:shadow-xl rounded-xl text-center m-7">
+                            <h3 className="text-xl font-semibold mb-2">Jane Smith</h3>
+                            <h4 className="text-gray-600 text-sm mb-4">Europe</h4>
+                            <p className="text-gray-800">The product descriptions are detailed and accurate, providing all the necessary information I need to make informed purchase decisions. I appreciate the high-quality product images from different angles, which give me a clear view of what I'm buying.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="bg-white p-4 shadow-lg hover:shadow-xl rounded-xl text-center m-7">
+                            <h3 className="text-xl font-semibold mb-2">Emtiaj Pabel</h3>
+                            <h4 className="text-gray-600 text-sm mb-4">Pabna, Bangladesh</h4>
+                            <p className="text-gray-800">The checkout process is smooth and secure. I never had any issues with payments, and the site offers various payment options to choose from, including credit cards, digital wallets, and bank transfers. The order tracking feature is also reliable.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="bg-white p-4 shadow-lg hover:shadow-xl rounded-xl text-center m-7">
+                            <h3 className="text-xl font-semibold mb-2">Jorina Madam</h3>
+                            <h4 className="text-gray-600 text-sm mb-4">Rongpur, Bangladesh</h4>
+                            <p className="text-gray-800">Moreover, the customer support team is responsive and helpful. I had a few inquiries and they promptly addressed my concerns, ensuring a positive shopping experience. The return and refund process is hassle-free as well, showing the site's commitment to customer satisfaction.</p>
+                        </div>
+                    </SwiperSlide>
+                </div>
             </Swiper>
         </div>
     );
