a134008281c3e26348c677f701c91007dc464d16
mdfaysal <faysal7830@gmail.com>
2023-05-23 22:07:03 +0600

    cart done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import logo from '../Photos/E_Store_logo.png';
 import { CartContext } from './Products';
 
 const Navbar = () => {
     const cartItems = useContext(CartContext);
-    console.log(cartItems);
+    // console.log(cartItems);
+
+
+    const subtotal = cartItems.cartItems.reduce((total, item) => total + item.price, 0);
+
+
     return (
         <div>
             <div className="navbar bg-base-100 fixed z-10 max-sm:px-5 px-10 mx-auto top-0 shadow">
                 <div className="flex-1">
                     <img className='h-16' src={logo} alt="" />
-                    <h2>{cartItems?.name}</h2>
                 </div>
                 <div className="flex-none">
                     <div className="flex-none max-sm:hidden">
@@ -34,15 +38,24 @@ const Navbar = () => {
                         <label tabIndex="0" className="btn btn-ghost btn-circle">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item">8</span>
+                                <span className="badge badge-sm indicator-item">{cartItems.cartItems.length}</span>
                             </div>
                         </label>
                         <div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
                             <div className="card-body">
-                                <span className="font-bold text-lg">8 Items</span>
-                                <span className="text-info">Subtotal: $999</span>
+                                <span className="font-bold text-lg">{cartItems.cartItems.length} Items</span>
+                                <ul>
+                                    {
+                                        cartItems.cartItems.map(cartItem => {
+                                            return(
+                                                <li>{cartItem.name} - ${cartItem.price}</li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                                <span className="text-info">Subtotal: ${subtotal}</span>
                                 <div className="card-actions">
-                                    <button className="btn btn-primary btn-block">View cart</button>
+                                    <button className="btn btn-primary btn-block">Place Order</button>
                                 </div>
                             </div>
                         </div>
